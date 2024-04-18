094383a89eeba6c06526f8fa9d0549fa0b836d7a
Nikita Grigorev <pettyaelse@gmail.com>
2024-04-18 23:33:03 +0700

    w

--- source/main.cpp
+++ source/main.cpp
@@ -1,22 +1,22 @@
 
 ///////////////**************new**************////////////////////
 
-//Include and link appropriate libraries and headers//
+// Include and link appropriate libraries and headers//
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dx11.lib")
 #pragma comment(lib, "d3dx10.lib")
 
-#include <windows.h>
 #include <d3d11.h>
-#include <d3dx11.h>
 #include <d3dx10.h>
+#include <d3dx11.h>
+#include <windows.h>
 #include <xnamath.h>
 
-//Global Declarations//
-IDXGISwapChain* SwapChain;
-ID3D11Device* d3d11Device;
-ID3D11DeviceContext* d3d11DevCon;
-ID3D11RenderTargetView* renderTargetView;
+// Global Declarations//
+IDXGISwapChain *SwapChain;
+ID3D11Device *d3d11Device;
+ID3D11DeviceContext *d3d11DevCon;
+ID3D11RenderTargetView *renderTargetView;
 
 float red = 0.0f;
 float green = 0.0f;
@@ -30,12 +30,12 @@ int colormodb = 1;
 LPCTSTR WndClassName = L"firstwindow";
 HWND hwnd = NULL;
 
-const int Width  = 300;
+const int Width = 300;
 const int Height = 300;
 
 ///////////////**************new**************////////////////////
 
-//Function Prototypes//
+// Function Prototypes//
 bool InitializeDirect3d11App(HINSTANCE hInstance);
 void ReleaseObjects();
 bool InitScene();
@@ -44,44 +44,33 @@ void DrawScene();
 
 ///////////////**************new**************////////////////////
 
-bool InitializeWindow(HINSTANCE hInstance,
-    int ShowWnd,
-    int width, int height,
-    bool windowed);
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool windowed);
 int messageloop();
 
-LRESULT CALLBACK WndProc(HWND hWnd,
-    UINT msg,
-    WPARAM wParam,
-    LPARAM lParam);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 
-int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
-    HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine,
-    int nShowCmd)
+int WINAPI WinMain(HINSTANCE hInstance, // Main windows function
+    HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 
-    if(!InitializeWindow(hInstance, nShowCmd, Width, Height, true))
+    if (!InitializeWindow(hInstance, nShowCmd, Width, Height, true))
     {
-        MessageBox(0, L"Window Initialization - Failed",
-            L"Error", MB_OK);
+        MessageBox(0, L"Window Initialization - Failed", L"Error", MB_OK);
         return 0;
     }
 
     ///////////////**************new**************////////////////////
 
-    if(!InitializeDirect3d11App(hInstance))    //Initialize Direct3D
+    if (!InitializeDirect3d11App(hInstance)) // Initialize Direct3D
     {
-        MessageBox(0, L"Direct3D Initialization - Failed",
-            L"Error", MB_OK);
+        MessageBox(0, L"Direct3D Initialization - Failed", L"Error", MB_OK);
         return 0;
     }
 
-    if(!InitScene())    //Initialize our scene
+    if (!InitScene()) // Initialize our scene
     {
-        MessageBox(0, L"Scene Initialization - Failed",
-            L"Error", MB_OK);
+        MessageBox(0, L"Scene Initialization - Failed", L"Error", MB_OK);
         return 0;
     }
 
@@ -94,12 +83,10 @@ int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
     return 0;
 }
 
-bool InitializeWindow(HINSTANCE hInstance,
-    int ShowWnd,
-    int width, int height,
-    bool windowed)
+bool InitializeWindow(HINSTANCE hInstance, int ShowWnd, int width, int height, bool windowed)
 {
-    typedef struct _WNDCLASS {
+    typedef struct _WNDCLASS
+    {
         UINT cbSize;
         UINT style;
         WNDPROC lpfnWndProc;
@@ -130,28 +117,16 @@ bool InitializeWindow(HINSTANCE hInstance,
 
     if (!RegisterClassEx(&wc))
     {
-        MessageBox(NULL, L"Error registering class",
-            L"Error", MB_OK | MB_ICONERROR);
+        MessageBox(NULL, L"Error registering class", L"Error", MB_OK | MB_ICONERROR);
         return 1;
     }
 
-    hwnd = CreateWindowEx(
-        NULL,
-        WndClassName,
-        L"Window Title",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        width, height,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-    );
+    hwnd = CreateWindowEx(NULL, WndClassName, L"Window Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+        CW_USEDEFAULT, width, height, NULL, NULL, hInstance, NULL);
 
     if (!hwnd)
     {
-        MessageBox(NULL, L"Error creating window",
-            L"Error", MB_OK | MB_ICONERROR);
+        MessageBox(NULL, L"Error creating window", L"Error", MB_OK | MB_ICONERROR);
         return 1;
     }
 
@@ -165,7 +140,7 @@ bool InitializeWindow(HINSTANCE hInstance,
 
 bool InitializeDirect3d11App(HINSTANCE hInstance)
 {
-    //Describe our Buffer
+    // Describe our Buffer
     DXGI_MODE_DESC bufferDesc;
 
     ZeroMemory(&bufferDesc, sizeof(DXGI_MODE_DESC));
@@ -178,7 +153,7 @@ bool InitializeDirect3d11App(HINSTANCE hInstance)
     bufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
     bufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
 
-    //Describe our SwapChain
+    // Describe our SwapChain
     DXGI_SWAP_CHAIN_DESC swapChainDesc;
 
     ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
@@ -193,27 +168,27 @@ bool InitializeDirect3d11App(HINSTANCE hInstance)
     swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 
 
-    //Create our SwapChain
+    // Create our SwapChain
     D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, NULL, NULL,
         D3D11_SDK_VERSION, &swapChainDesc, &SwapChain, &d3d11Device, NULL, &d3d11DevCon);
 
-    //Create our BackBuffer
-    ID3D11Texture2D* BackBuffer;
-    SwapChain->GetBuffer( 0, __uuidof( ID3D11Texture2D ), (void**)&BackBuffer );
+    // Create our BackBuffer
+    ID3D11Texture2D *BackBuffer;
+    SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void **)&BackBuffer);
 
-    //Create our Render Target
-    d3d11Device->CreateRenderTargetView( BackBuffer, NULL, &renderTargetView );
+    // Create our Render Target
+    d3d11Device->CreateRenderTargetView(BackBuffer, NULL, &renderTargetView);
     BackBuffer->Release();
 
-    //Set our Render Target
-    d3d11DevCon->OMSetRenderTargets( 1, &renderTargetView, NULL );
+    // Set our Render Target
+    d3d11DevCon->OMSetRenderTargets(1, &renderTargetView, NULL);
 
     return true;
 }
 
 void ReleaseObjects()
 {
-    //Release the COM Objects we created
+    // Release the COM Objects we created
     SwapChain->Release();
     d3d11Device->Release();
     d3d11DevCon->Release();
@@ -226,53 +201,58 @@ bool InitScene()
 
 void UpdateScene()
 {
-    //Update the colors of our scene
+    // Update the colors of our scene
     red += colormodr * 0.00005f;
     green += colormodg * 0.00002f;
     blue += colormodb * 0.00001f;
 
-    if(red >= 1.0f || red <= 0.0f)
+    if (red >= 1.0f || red <= 0.0f)
+    {
         colormodr *= -1;
-    if(green >= 1.0f || green <= 0.0f)
+    }
+    if (green >= 1.0f || green <= 0.0f)
+    {
         colormodg *= -1;
-    if(blue >= 1.0f || blue <= 0.0f)
+    }
+    if (blue >= 1.0f || blue <= 0.0f)
+    {
         colormodb *= -1;
+    }
 }
 
 void DrawScene()
 {
-    //Clear our backbuffer to the updated color
-    D3DXCOLOR bgColor( red, green, blue, 1.0f );
+    // Clear our backbuffer to the updated color
+    D3DXCOLOR bgColor(red, green, blue, 1.0f);
 
     d3d11DevCon->ClearRenderTargetView(renderTargetView, bgColor);
 
-    //Present the backbuffer to the screen
+    // Present the backbuffer to the screen
     SwapChain->Present(0, 0);
 }
 
 ///////////////**************new**************////////////////////
 
-int messageloop(){
+int messageloop()
+{
     MSG msg;
     ZeroMemory(&msg, sizeof(MSG));
-    while(true)
+    while (true)
     {
-        BOOL PeekMessageL(
-            LPMSG lpMsg,
-            HWND hWnd,
-            UINT wMsgFilterMin,
-            UINT wMsgFilterMax,
-            UINT wRemoveMsg
-        );
+        BOOL PeekMessageL(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilterMax,
+            UINT wRemoveMsg);
 
         if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
         {
             if (msg.message == WM_QUIT)
+            {
                 break;
+            }
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
-        else{
+        else
+        {
             ///////////////**************new**************////////////////////
             // run game code
 
@@ -285,25 +265,18 @@ int messageloop(){
     return msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd,
-    UINT msg,
-    WPARAM wParam,
-    LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch( msg )
+    switch (msg)
     {
     case WM_KEYDOWN:
-        if( wParam == VK_ESCAPE ){
+        if (wParam == VK_ESCAPE)
+        {
             DestroyWindow(hwnd);
         }
         return 0;
 
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
+    case WM_DESTROY: PostQuitMessage(0); return 0;
     }
-    return DefWindowProc(hwnd,
-        msg,
-        wParam,
-        lParam);
+    return DefWindowProc(hwnd, msg, wParam, lParam);
 }
